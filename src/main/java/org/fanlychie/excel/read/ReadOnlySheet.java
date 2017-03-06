69ab4b4a6211a9de5f6ee83e296be56f48ddc65a
fanlychie <fanlychie@yeah.net>
2017-03-06 17:35:27 +0800

    初始提交

---
+++ src/main/java/org/fanlychie/excel/read/ReadOnlySheet.java
@@ -0,0 +1,29 @@
+package org.fanlychie.excel.read;
+
+/**
+ * 只读的工作表
+ * Created by fanlychie on 2017/3/6.
+ */
+public class ReadOnlySheet {
+
+    /**
+     * 工作表索引
+     */
+    private int index;
+
+    /**
+     * 工作表名称
+     */
+    private String name;
+
+    /**
+     * 开始行号
+     */
+    private int beginRowNumber;
+
+    /**
+     * 结束行号
+     */
+    private int endRowNumber;
+
+}
--- src/main/java/org/fanlychie/excel/read/ReadableExcel.java
+++ src/main/java/org/fanlychie/excel/read/ReadableExcel.java
@@ -1,5 +1,6 @@
 package org.fanlychie.excel.read;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.fanlychie.reflection.exception.ExcelCastException;
@@ -15,6 +16,21 @@ import java.util.List;
  * Created by fanlychie on 2017/3/5.
  */
 public class ReadableExcel {
+
+    /**
+     * 工作表
+     */
+    private Sheet sheet;
+
+    /**
+     * 工作表索引值
+     */
+    private int sheetIndex;
+
+    /**
+     * 工作表名称
+     */
+    private String sheetName;
 
     /**
      * Excel 文件输入流
@@ -52,10 +68,36 @@ public class ReadableExcel {
         this.inputStream = inputStream;
     }
 
+    /**
+     * 设置读取的工作表索引
+     * @param sheetIndex 读取的工作表索引值, 从0开始
+     * @return 返回当前对象
+     */
+    public ReadableExcel sheetIndex(int sheetIndex) {
+        this.sheetIndex = sheetIndex;
+        return this;
+    }
+
+    /**
+     * 设置读取的工作表名称
+     * @param sheetName 读取的工作表名称
+     * @return 返回当前对象
+     */
+    public ReadableExcel sheetName(String sheetName) {
+        this.sheetName = sheetName;
+        return this;
+    }
+
     public <T> List<T> parse(Class<T> targetClass) {
         try {
             // 工作薄
             Workbook workbook = WorkbookFactory.create(inputStream);
+            // 获取要解析的工作表
+            if (sheetName != null && !sheetName.isEmpty()) {
+                sheet = workbook.getSheet(sheetName);
+            } else {
+                sheet = workbook.getSheetAt(sheetIndex);
+            }
         } catch (Throwable e) {
             throw new ExcelCastException(e);
         }
